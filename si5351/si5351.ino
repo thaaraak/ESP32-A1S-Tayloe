7cfd985a27ca70fcf8b4c7c16ca4c0606153e98d
thaaraak <60319319+thaaraak@users.noreply.github.com>
2022-11-09 12:16:54 -0500

    Update si5351.ino

--- si5351/si5351.ino
+++ si5351/si5351.ino
@@ -1,42 +1,76 @@
-#include <Adafruit_SI5351.h>
+#include <si5351.h>
 
-Adafruit_SI5351 clockgen = Adafruit_SI5351();
+Si5351 *si5351;
+TwoWire wire(0);
 
 /**************************************************************************/
 /*
     Arduino setup function (automatically called at startup)
 */
 /**************************************************************************/
+
+
+int currentFrequency = -1;
+int lastMult = -1;
+
+void changeFrequency( int freq )
+{
+    int mult = 0;
+    currentFrequency = freq;
+
+    if ( freq < 5000000 )
+      mult = 150;
+    else if ( freq < 6000000 )
+      mult = 120;
+    else if ( freq < 8000000 )
+      mult = 100;
+    else if ( freq < 11000000 )
+      mult = 80;
+    else if ( freq < 15000000 )
+      mult = 50;
+    else if ( freq < 22000000 )
+      mult = 40;
+    else if ( freq < 30000000 )
+      mult = 30;
+    else if ( freq < 40000000 )
+      mult = 20;
+    else if ( freq < 50000000 )
+      mult = 15;
+
+    uint64_t f = freq * 100ULL;
+    uint64_t pllFreq = freq * mult * 100ULL;
+
+    si5351->set_freq_manual(f, pllFreq, SI5351_CLK0);
+    si5351->set_freq_manual(f, pllFreq, SI5351_CLK2);
+
+    if ( mult != lastMult )
+    {
+      si5351->set_phase(SI5351_CLK0, 0);
+      si5351->set_phase(SI5351_CLK2, mult);
+      si5351->pll_reset(SI5351_PLLA);
+      si5351->update_status();
+
+      lastMult = mult;
+    }
+}
+
+void setupSynth()
+{
+  si5351 = new Si5351( &wire );
+  si5351->init( SI5351_CRYSTAL_LOAD_8PF, 0, 0);
+}
+
+
 void setup(void)
 {
   Serial.begin(115200);
-  Serial.println("Si5351 Clockgen Test"); Serial.println("");
+  Serial.println("Si5351 Clockgen Test");
 
-
-  TwoWire wire(0);
   wire.setPins( 23, 19 );
-  
-  /* Initialise the sensor */
-  if (clockgen.begin( &wire ) != ERROR_NONE)
-  {
-    /* There was a problem detecting the IC ... check your connections */
-    Serial.print("Ooops, no Si5351 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
 
-  Serial.println("OK!");
+  setupSynth();
+  changeFrequency(7000000);  
 
-  int mult = 50;
-  Serial.println("Set PLLA to 700MHz");
-  clockgen.setupPLLInt(SI5351_PLL_A, 30);
-  Serial.println("Set Output #0 to 7MHz");
-  clockgen.setupMultisynth(0, SI5351_PLL_A, mult, 0, 1);
-  clockgen.setupMultisynth(2, SI5351_PLL_A, mult, 0, 1);
-
-  clockgen.setPhase( 0, 0 );
-  clockgen.setPhase( 2, mult );
-
-  clockgen.enableOutputs(true);
 }
 
 /**************************************************************************/
